4700354e356c15374a1dab19dff559786164e0df
devin.dao <devin.dao@avepoint.com>
2022-04-21 22:11:04 +0700

    update package

---
+++ src/Util/ConnectionDB.java
@@ -0,0 +1,39 @@
+package Util;
+
+import java.sql.*;
+
+public class ConnectionDB {
+    public static String URL = "jdbc:mysql://localhost:3306/oop20212";
+    public static String USER = "root";
+    public static String PASSWORD = "123456";
+
+    public static Connection openConnection(){
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    public static void closeConnection(Connection conn) {
+        if(conn!=null) {
+            try {
+                conn.close();
+            }catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws SQLException {
+        Connection connection = ConnectionDB.openConnection();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from book");
+        while (resultSet.next()){
+            System.out.println(resultSet.getString("id"));
+        }
+        ConnectionDB.closeConnection(connection);
+    }
+}
